f4a2cb6791eaeea1441b94e602f18e78bbc827e1
nailyk <jenkins@nailyk.fr>
2019-05-05 17:58:42 +0200

    common-treble: Treble: Power qti does not exist
     * Qti power manager does not exist on omni.
     * Use omni services instead.
    
    Signed-off-by: nailyk <jenkins@nailyk.fr>
    Change-Id: I544b57595130bcb77d18bd038b45d9b65ed5758c

--- treble.mk
+++ treble.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
 
 # POWER
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl\
+    android.hardware.power@1.0-service
 
 # HEALTH
 PRODUCT_PACKAGES += \
